35ddb9b877682046e8ae6246c7f1b4b8bd2339d0
ilya <ilasmyga26@gmail.com>
2023-10-04 19:03:53 +0300

    initial commit

---
+++ src/Exercise3.java
@@ -0,0 +1,17 @@
+public class Exercise3 {
+    public static void main(String [] args){
+        //3.4.1
+        int pay = 24_000, score = 70;
+        if (score>90){
+            System.out.println(pay*3/100+pay);//24.720
+        }
+        else{
+            System.out.println(pay*1/100+pay);//24.240
+        }
+
+        //3.4.2
+        // в (a) выведится оба предполагаемых исхода
+        // в (b) если number = 35 то остаток останется и условие нарушится
+        // при наличии else вывод будет: is odd
+    }
+}
